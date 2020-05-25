cdac3c2367ede6bfbddc29009e3c5bb8658cb215
Ilya Zub <zaoooza92@gmail.com>
2020-05-25 14:01:35 +0300

    Commit auto-prettified code

--- components/markdown.js
+++ components/markdown.js
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from "@/components/code-block";
-import markdownStyles from "@/components/markdown-styles.module.css"
+import markdownStyles from "@/components/markdown-styles.module.css";
 
 export default function Markdown({ value }) {
   return (
     <ReactMarkdown
-      className={markdownStyles['markdown']}
+      className={markdownStyles["markdown"]}
       source={value}
       renderers={{
         code: CodeBlock,
